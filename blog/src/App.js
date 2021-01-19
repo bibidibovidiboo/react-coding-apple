0a1256b2083ff93f52e362ffebce28f5ec5e3ef0
bibidibovidiboo <66943451+bibidibovidiboo@users.noreply.github.com>
2021-01-19 22:50:48 +0900

    update blog

---
+++ blog/src/App.js
@@ -0,0 +1,23 @@
+/* eslint-disable */
+import React, { useState } from 'react';
+import './App.css';
+
+function App(){
+ 
+  let [글제목, 글제목변경] = useState('남자 코트 추천');
+  let posts = '강남 고기 맛집';
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <div>개발 blog</div>
+      </div>
+      <div className="list">
+        <h3>{ 글제목 }</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+    </div>
+  )
+}
+
+export default App;
