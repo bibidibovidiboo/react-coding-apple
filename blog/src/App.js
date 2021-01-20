d411c2b340e1475b257ee3f5691f2102ccf4250d
bibidibovidiboo <66943451+bibidibovidiboo@users.noreply.github.com>
2021-01-20 19:04:36 +0900

    Update App.js

--- blog/src/App.js
+++ blog/src/App.js
@@ -3,17 +3,36 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App(){
- 
-  let [글제목, 글제목변경] = useState('남자 코트 추천');
+  
+  let [글제목, 글제목변경] = useState(['남자코트 추천','강남 우동맛집','파이썬 독학']);
+  let [따봉, 따봉변경] = useState(0);
   let posts = '강남 고기 맛집';
+  
+  function 제목바꾸기(){
+    var newArray = [...글제목]; // 복사 먼저 하기 
+    newArray[0] = '여자코트 추천'; // 데이터를 가공
+    글제목변경( newArray ); // 다시 넣어준다
+  }
+
   return (
     <div className="App">
       <div className="black-nav">
-        <div>개발 blog</div>
+        <div>개발 Blog</div>
+      </div>
+      <button onClick={ 제목바꾸기 }>버튼</button>
+      <div className="list">
+        <h3>{ 글제목[0] }<span onClick={ () => { 따봉변경(따봉 +1) } }>👍</span>{따봉}</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
       </div>
       <div className="list">
-        <h3>{ 글제목 }</h3>
-        <p>2월 17일 발행</p>
+        <h3>{ 글제목[1] }</h3>
+        <p>2월 18일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3>{ 글제목[2] }</h3>
+        <p>2월 19일 발행</p>
         <hr/>
       </div>
     </div>
