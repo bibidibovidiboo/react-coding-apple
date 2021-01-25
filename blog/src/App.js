e71f758f958b264aa0855bf938e4e21b2a07a1ee
bibidibovidiboo <66943451+bibidibovidiboo@users.noreply.github.com>
2021-01-25 22:15:57 +0900

    리액트 수업 실습

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,11 +2,12 @@
 import React, { useState } from 'react';
 import './App.css';
 
-
 function App(){
   
   let [글제목, 글제목변경] = useState(['남자코트 추천','강남 우동맛집','파이썬 독학']);
   let [따봉, 따봉변경] = useState(0);
+
+  let [modal, modal변경] = useState(false);
 
   let posts = '강남 고기 맛집';
   
@@ -21,7 +22,7 @@ function App(){
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <button onClick={ 제목바꾸기 }>버튼</button>
+      {/* <button onClick={ 제목바꾸기 }>버튼</button> */}
       <div className="list">
         <h3>{ 글제목[0] }<span onClick={ () => { 따봉변경(따봉 +1) } }>👍</span>{따봉}</h3>
         <p>2월 17일 발행</p>
@@ -33,13 +34,46 @@ function App(){
         <hr/>
       </div>
       <div className="list">
-        <h3>{ 글제목[2] }</h3>
+        <h3 onClick={ ()=>{ modal변경(true) } } > { 글제목[2] }</h3>
         <p>2월 19일 발행</p>
         <hr/>
       </div>
+
+      {
+        modal == true 
+        ? <Modal></Modal>
+        : null
+      }
+
+      {
+        글제목.map(function(a){
+          return (
+            <div className="list">
+              <h3>{ a }</h3>
+              <p>2월 17일 발행</p>
+              <hr/>
+            </div>
+          )
+        })
+      }
+      
     </div>
   )
 }
 
+// Component 
+
+function Modal(){
+  return (
+    <>
+    <div className="modal">
+        <h2>제목</h2>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+    <div></div>
+    </>
+  )
+}
 
 export default App;
