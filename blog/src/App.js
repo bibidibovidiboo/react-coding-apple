3ae4b42674ffb963eceab3503d2aae3201c761af
bibidibovidiboo <66943451+bibidibovidiboo@users.noreply.github.com>
2021-01-26 19:11:03 +0900

    글쓰기 기능 추가

--- blog/src/App.js
+++ blog/src/App.js
@@ -6,10 +6,11 @@ function App(){
   
   let [글제목, 글제목변경] = useState(['남자코트 추천','강남 우동맛집','파이썬 독학']);
   let [따봉, 따봉변경] = useState(0);
-
   let [modal, modal변경] = useState(false);
+  let [누른제목, 누른제목변경] = useState(0);
 
-  let posts = '강남 고기 맛집';
+  let [입력값, 입력값변경] = useState('');
+
   
   function 제목바꾸기(){
     var newArray = [...글제목]; // 복사 먼저 하기 
@@ -23,7 +24,7 @@ function App(){
         <div>개발 Blog</div>
       </div>
       {/* <button onClick={ 제목바꾸기 }>버튼</button> */}
-      <div className="list">
+      {/* <div className="list">
         <h3>{ 글제목[0] }<span onClick={ () => { 따봉변경(따봉 +1) } }>👍</span>{따봉}</h3>
         <p>2월 17일 발행</p>
         <hr/>
@@ -37,25 +38,43 @@ function App(){
         <h3 onClick={ ()=>{ modal변경(true) } } > { 글제목[2] }</h3>
         <p>2월 19일 발행</p>
         <hr/>
-      </div>
+      </div> */}
 
       {
-        modal == true 
-        ? <Modal></Modal>
-        : null
-      }
-
-      {
-        글제목.map(function(a){
+        글제목.map(function(글, i){
           return (
-            <div className="list">
-              <h3>{ a }</h3>
+            <div className="list" key={i}>
+              <h3 onClick={ () => { 누른제목변경(i) } } >{ 글 }<span onClick={ () => { 따봉변경(따봉 +1) } }>👍</span> {따봉} </h3>
               <p>2월 17일 발행</p>
               <hr/>
             </div>
           )
         })
       }
+
+      {/* <button onClick={ () => { 누른제목변경(0) } }>버튼1</button>
+      <button onClick={ () => { 누른제목변경(1) } }>버튼2</button>
+      <button onClick={ () => { 누른제목변경(2) } }>버튼3</button> */}
+
+      
+      <div className="publish">
+        <input onChange={ (e) => { 입력값변경(e.target.value) } }/>
+        <button onClick={ () => {
+          var arrayCopy = [...글제목];
+          arrayCopy.unshift(입력값)
+          글제목변경( arrayCopy );
+        } }>저장</button>
+      </div>
+
+      <button onClick={ () => { modal변경(!modal)}}>열고닫기</button>
+
+      {
+        modal == true 
+        ? <Modal 글제목={글제목} 누른제목={누른제목}></Modal>
+        : null
+      }
+
+      
       
     </div>
   )
@@ -63,11 +82,11 @@ function App(){
 
 // Component 
 
-function Modal(){
+function Modal(props){
   return (
     <>
     <div className="modal">
-        <h2>제목</h2>
+        <h2>{ props.글제목[props.누른제목] }</h2>
         <p>날짜</p>
         <p>상세내용</p>
       </div>
