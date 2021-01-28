5a1b2c611f516d0e74b29b7da4127f5347dad4de
bibidibovidiboo <66943451+bibidibovidiboo@users.noreply.github.com>
2021-01-28 20:08:29 +0900

    상세페이지 추가

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,8 +1,11 @@
 /* eslint-disable */
 import { useState } from "react";
 import { Navbar, Nav, NavDropdown, Button, Jumbotron } from "react-bootstrap";
-import Data from "./data.js";
 import "./App.css";
+import Data from "./data.js";
+import Detail from "./Detail.js";
+
+import { Link, Route, Switch } from "react-router-dom";
 
 function App() {
   let [shoes, shoes변경] = useState(Data);
@@ -14,8 +17,12 @@ function App() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link>
+              <Link to="/">Home</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/detail">Detail</Link>
+            </Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
@@ -30,24 +37,39 @@ function App() {
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      <Jumbotron className="background">
-        <h1>20% season OFF</h1>
-        <p>
-          This is a simple hero unit, a simple jumbotron-style component for
-          calling extra attention to featured content or information.
-        </p>
-        <p>
-          <Button variant="primary">Learn more</Button>
-        </p>
-      </Jumbotron>
 
-      <div className="container">
-        <div className="row">
-          {shoes.map((a, i) => {
-            return <Card shoes={shoes[i]} i={i} key={i} />;
-          })}
-        </div>
-      </div>
+      <Switch>
+        <Route exact path="/">
+          <Jumbotron className="background">
+            <h1>20% season OFF</h1>
+            <p>
+              This is a simple hero unit, a simple jumbotron-style component for
+              calling extra attention to featured content or information.
+            </p>
+            <p>
+              <Button variant="primary">Learn more</Button>
+            </p>
+          </Jumbotron>
+
+          <div className="container">
+            <div className="row">
+              {shoes.map((a, i) => {
+                return <Card shoes={shoes[i]} i={i} key={i} />;
+              })}
+            </div>
+          </div>
+        </Route>
+
+        <Route path="/detail/:id">
+          <Detail shoes={shoes} />
+        </Route>
+
+        <Route path="/:id">
+          <div>아무거나 적었을 때 이거 보여주셈</div>
+        </Route>
+      </Switch>
+
+      {/* <Route path="/어쩌구" component={Modal}></Route> */}
     </div>
   );
 }
